869aea7ebfd7ae6f1f7d215a21564b2f5e31b374
VRushLP <rmfarc@uw.edu>
2016-08-07 11:40:02 -0700

    Filters, changes before merge
    Scrapeler now always outputs keywords to a file.
    Length of output is still up to user.
    Removed sleep command.
    Removed random page limit command.
    Removed now unnecessary imports.
    Args are now parsed from batch files better
    Refactored scraping code to work with filters.

--- scrapeler.py
+++ scrapeler.py
@@ -5,7 +5,7 @@ import re
 import os
 import datetime
 import time
-import random as _rand
+import random
 import urllib
 import requests
 import codecs
@@ -13,8 +13,6 @@ import sys
 
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-from io import BytesIO
-from PIL import Image
 
 main_url_base = r'http://gelbooru.com/index.php?page=post&s=list&tags={url_tags}&pid={pid}'
 grab_url_base = r'http://gelbooru.com//images/{0}/{1}/{2}'
@@ -40,79 +38,68 @@ def get_soup(url):
 
 
 def parse_scrapeler_args(batch_args=None):
-    scrapeler_args = {}
+    # sys.argv[0] is always 'scrapeler.py'
+    raw_args = batch_args.split() if batch_args is not None else sys.argv[1:]
 
     parser = argparse.ArgumentParser(description='Scrape a booru-style image database. '
                                                  'At least one tag is required to scrape. Choose carefully!')
     parser.add_argument("tags", type=str,
                         help="Enter the tags you want to scrape.\nAt least 1 tag argument is required.",
                         nargs='+',)
-    parser.add_argument("-e", "--exclude", type=str, help="Enter tags you want to avoid.",
-                        nargs='*', default=None)
+    parser.add_argument("-e", "--exclude", default=None,
+                        nargs='*', type=str, help="Tags that are explicitly avoided in urls. Helps to narrow down searches.")
+    parser.add_argument("-f", "--filter", type=str, default=None,
+                        nargs='*', help="If an image has a tag in this list, Scrapeler will not save it.")
     parser.add_argument("-d", "--dir", help="The directory you want the images saved to.", nargs = '?')
     parser.add_argument("-p", "--page", type=int, help="Page you want to start the scraping on", default=1, nargs='?')
     parser.add_argument("-c", "--kwcount", type=int, default=25,
-                        help="The number of counted keywords reported. Defaults to 25. If this is 0, Scrapeler will not count keywords. Set this to -1 to grab all related keywords.")
-    parser.add_argument("-f", "--keywordfile", default=False, action='store_true',
-                        help="Whether or not to store keyword counts in a file. If not specified, Scrapeler will report to the console.")
+                        help="The number of counted keywords reported. Defaults to 25. If this is 0, Scrapeler will not count keywords. Set this to -1 to grab all scanned keywords.")
     parser.add_argument("--pagelimit", type=int, default=-1, help='How many pages to scan before stopping. If below 1, Scrapeler will continue until it finds a page with fewer than maximum images.')
-    parser.add_argument("--sleep", default=False, action='store_true', help='If on, Scrapeler will sleep randomly trying to disguise itself.')
-    parser.add_argument("--randompagelimit", default=False, action='store_true', help='If on, Scrapeler will stop when it finds no more images or randomly between 10 and 30 pages.')
     parser.add_argument("--scanonly", default=False, action='store_true', help='If on, images will not be saved, but you still collect keyword data.')
     parser.add_argument("--shortcircuit", default=False, action='store_true', help='If on, Scrapeler will stop scraping if it finds nothing on a page that you haven\'t already saved. Does nothing if --scanonly is on.')
-    parser.add_argument("--batch", default=None, type=argparse.FileType('r'))
+    parser.add_argument("--batch", default=None, type=argparse.FileType('r'), help="Pass a file that contains additional Scrapeler queries here.")
 
-    if not batch_args:
-        args = parser.parse_args()
+    parsed_args = parser.parse_args(raw_args)
+
+    if parsed_args.dir is not None:
+        directory = parsed_args.dir
     else:
-        args = parser.parse_args(batch_args.split())
-        assert args.batch is None
+        directory = datetime.datetime.now().strftime('{0} %Y%m%d_%H%M').format(parsed_args.tags[0])
 
-    if args.dir is not None:
-        directory = args.dir
-        save_path= os.path.abspath(directory)
-    else:
-        directory = datetime.datetime.now().strftime('{0} %Y%m%d_%H%M').format(args.tags[0])
-        save_path= os.path.abspath(directory)
-
-    # Only make the directories if a response file is not about to be invoked.
+    save_path = os.path.abspath(directory)
     if not os.path.exists(save_path):
         os.makedirs(save_path)
 
     temp_include = []
-    for tag in args.tags:
+    for tag in parsed_args.tags:
         temp_include.append(urllib.parse.quote(tag))
-
     include_tags= ''.join('%s+' % x.replace('&', '%26').replace(':','%3a') for x in temp_include)
 
-    if args.exclude is not None:
+    if parsed_args.exclude is not None:
         temp_exclude = []
-        for tag in args.exclude:
+        for tag in parsed_args.exclude:
             temp_exclude.append(urllib.parse.quote(tag))
-
         exclude_tags = ''.join('-%s+' % x.replace('&', '%26').replace(':','%3a') for x in temp_exclude)
     else:
         exclude_tags = ''
 
-    base_delay = 4
+    if parsed_args.filter:
+        filtered_tags = {item: 1 for item in parsed_args.filter}
 
-    scrapeler_args['tags'] = args.tags
-    scrapeler_args['exclude'] = args.exclude if args.exclude is not None else []
-    scrapeler_args['url_tags'] = (include_tags + exclude_tags)[:-1]
-    scrapeler_args['scrape_save_directory'] = save_path
-    scrapeler_args['page'] = args.page
-    scrapeler_args['kwfile'] = args.keywordfile
-    scrapeler_args['kwcount'] = args.kwcount if args.kwcount >= -1 else 0
-    scrapeler_args['scanonly'] = args.scanonly
-    scrapeler_args['base_delay'] = base_delay
-    scrapeler_args['sleep'] = args.sleep
-    scrapeler_args['short'] = args.shortcircuit
-    scrapeler_args['batch'] = args.batch
-
-    if args.randompagelimit:
-        scrapeler_args['pagelimit'] = _rand.randint(10,30)
-    else:
-        scrapeler_args['pagelimit'] = args.pagelimit if args.pagelimit > 0 else -1
+    scrapeler_args = {
+        'tags': parsed_args.tags,
+        'exclude': parsed_args.exclude if parsed_args.exclude is not None else [],
+        'filter': filtered_tags if parsed_args.filter is not None else {},
+        'url_tags': (include_tags + exclude_tags)[:-1],
+        'scrape_save_directory': save_path,
+        'kwcount': parsed_args.kwcount if parsed_args.kwcount >= -1 else 0,
+        'page': parsed_args.page,
+        'pagelimit': (parsed_args.pagelimit + parsed_args.page if parsed_args.pagelimit > 0 else -1),
+        'scanonly': parsed_args.scanonly,
+        'base_delay': 4,
+        'short': parsed_args.shortcircuit,
+        'batch': parsed_args.batch,
+    }
 
     return scrapeler_args
 
@@ -124,6 +111,7 @@ def route_through_subpage(directory_page, referer_id, image_file_path):
             'User-Agent': UserAgent().firefox,
             'Referer': directory_page,
         })
+
     if not response.status_code == 404:
         soup = BeautifulSoup(response.content, "html5lib", from_encoding=response.encoding)
         try:
@@ -140,39 +128,42 @@ def route_through_subpage(directory_page, referer_id, image_file_path):
                 result = result[:result.find('"')]
                 current_img = split_url[0] + '.' + result
 
-            # this logic is fucked by I don't care
+            # this logic is fucked but I don't care
             extension = current_img.split('?')[0][-5:].split('.')[1]
             # image file path always starts life as a .jpg
             image_file_path = image_file_path[:-3] + extension
 
             if not os.path.exists(image_file_path):
-                delay = 4 + _rand.uniform(3, 4)
+                delay = 4 + random.uniform(2, 4)
                 time.sleep(delay)
                 ret = save_image(referer_id, current_img, image_file_path)
             else:
                 print('{0} skipped: Already saved.'.format(current_img))
         except AttributeError as ae:
+            # TODO Refactor this to be less redundant.
             img_tag = soup.find('source')
             current_img = img_tag.attrs['src']
             image_file_path = image_file_path[:-3] + 'webm'
             if not os.path.exists(image_file_path):
-                delay = 4 + _rand.uniform(3, 4)
+                delay = 4 + random.uniform(3, 4)
                 time.sleep(delay)
                 ret = save_image(referer_id, current_img, image_file_path)
             else:
                 print('{0} skipped: Already saved.'.format(current_img))
 
         except Exception as e:
-            print(e)
+            print('Unhandled exception: {}'.format(e))
     return ret
 
 
 def save_image(referer_id, current_img, save_to):
     with requests.Session() as sess:
-        response = sess.get(current_img, data=None, stream=True, headers={
-            'User-Agent': UserAgent().firefox,
-            'Referer': referer_id,
-        })
+        response = sess.get(current_img, data=None, stream=True,
+                            headers={
+                                'User-Agent': UserAgent().firefox,
+                                'Referer': referer_id,
+                            }
+        )
         try:
             if response.status_code == 200:
                 with open(save_to, 'wb') as f:
@@ -181,86 +172,82 @@ def save_image(referer_id, current_img, save_to):
                             f.write(chunk)
                     print(current_img)
                     return 1
+
         except Exception as e:
             print(e)
             return 0
 
 
 def scrape_booru(scrapeler_args):
-
     related_tags = {}
-    found = 0
+    found_imgs = 0
     total_saved_imgs = 0
-    page = scrapeler_args['page']
     url_tags = scrapeler_args['url_tags']
-    if scrapeler_args['sleep']:
-        next_sleep = _rand.randint(7200, 10800)  # 2 to 3 hours til sleeping.
-        print('Will sleep in {0} seconds'.format(next_sleep))
-    keep_scraping = True
-    if scrapeler_args['pagelimit']:
-        final_page = scrapeler_args['pagelimit'] + page
-    else:
-        final_page = -1
 
-    timestamp = datetime.datetime.now()
+    page = scrapeler_args['page']
+    final_page = scrapeler_args['pagelimit']
+
+    keep_scraping = True
     while keep_scraping:
         saved_imgs = 0
-        delay = scrapeler_args['base_delay'] + _rand.uniform(2,4)
+        delay = scrapeler_args['base_delay'] + random.uniform(2, 4)
         time.sleep(delay)
-        scrape_url = main_url_base.format(url_tags=url_tags ,pid=str(42* (page-1)))
-        scrape_soup = get_soup(scrape_url)
+        scrape_url = main_url_base.format(url_tags=url_tags, pid=str(42 * (page-1)))
         print('\nScraping: {0}, (page {1})'.format(scrape_url, page))
+
+        scrape_soup = get_soup(scrape_url)
         results = scrape_soup.findAll('img', class_='preview')
-        found += len(results)
+
+        found_imgs += len(results)
         if len(results) < 42:
             keep_scraping = False
 
-        print('{0} results on page ({1} found, {2} saved.)\n'.format(len(results), found, total_saved_imgs))
+        print('{0} results on page\n'.format(len(results)))
 
         for result in results:
-            if scrapeler_args['kwcount'] != 0:
-                for tag in result.attrs['title'].split():
-                    if tag in related_tags and 'score' not in tag:
-                        related_tags[tag] += 1
-                    else:
-                        related_tags[tag] = 1
+            save_current = True
+            filter_reasons = []
+            for tag in result.attrs['title'].split():
+                if tag in related_tags and 'score' not in tag:
+                    related_tags[tag] += 1
+                else:
+                    related_tags[tag] = 1
+                if tag in scrapeler_args['filter']:
+                    save_current = False
+                    filter_reasons.append(tag)
 
             if not scrapeler_args['scanonly']:
                 img_fn = id_regex.search(result.attrs['src']).group(1)
                 refer_id = referer_regex.search(result.attrs['src']).group(0)[1:]
-                image_file_path = "{directory}\\{fn}".format(directory=scrapeler_args['scrape_save_directory'],
-                                                             fn=img_fn)
-                delay = scrapeler_args['base_delay'] + _rand.uniform(0,2)
-                time.sleep(delay)
-                saved_imgs += route_through_subpage(scrape_url, referer_base.format(refer_id), image_file_path)
+                if save_current:
+                    image_file_path = "{directory}\\{fn}".format(directory=scrapeler_args['scrape_save_directory'],
+                                                                 fn=img_fn)
+                    delay = scrapeler_args['base_delay'] + random.uniform(0, 2)
+                    time.sleep(delay)
+                    saved_imgs += route_through_subpage(scrape_url, referer_base.format(refer_id), image_file_path)
+                elif not save_current:
+                    print('{0} was filtered. Matched: {1}.'.format(referer_base.format(refer_id), filter_reasons[:3]))
 
-                # todo if you scrape and find this tag: <title>Gelbooru - Intermission Ad</title>
-                # wait 15 seconds then try the page again
-        total_saved_imgs += saved_imgs
+                    # todo if you scrape and find this tag: <title>Gelbooru - Intermission Ad</title>
+                    # wait 15 seconds then try the page again
+
+        if not scrapeler_args['scanonly']:
+            total_saved_imgs += saved_imgs
+            print('{0} images saved for page {1}. ({2} images saved in total.)'.format(saved_imgs, page, total_saved_imgs))
 
         page += 1
         if -1 < final_page == page:
+            print('Page limit ({0}) was reached. Stopping.'.format(final_page))
             keep_scraping = False
 
         if scrapeler_args['short'] and not scrapeler_args['scanonly'] and saved_imgs == 0:
+            print('No images saved with shortcircuit on. Stopping.')
             keep_scraping = False
-
-        if keep_scraping and scrapeler_args['sleep']:
-            rn = datetime.datetime.now()
-            if rn - timestamp > datetime.timedelta(seconds=next_sleep):
-                print('Scrapeler entered sleep. ({0})'.format(datetime.datetime.now()))
-                long_delay = _rand.randint(1800, 3600) + _rand.uniform(0,1)  # .5 to 1 hours of sleeping
-                while datetime.datetime.now() < rn + datetime.timedelta(seconds=long_delay):
-                    time.sleep(120)
-                timestamp = datetime.datetime.now()
-                print('Scrapeler started scraping again. ({0})'.format(timestamp))
-                next_sleep = _rand.randint(7200, 10800)  # 2 to 3 hours til sleeping.
-                print('Will sleep in {0} seconds'.format(next_sleep))
 
     return related_tags
 
 
-def perform(scrapeler_args):
+def perform_gelbooru_scrape(scrapeler_args):
 
     print('\nArguments parsed as:')
     print('Include tags:', scrapeler_args['tags'])
@@ -276,7 +263,8 @@ def perform(scrapeler_args):
         kwcount = len(sorted_related_tags)
     else:
         kwcount = scrapeler_args['kwcount']
-    if scrapeler_args['kwfile']:
+
+    if scrapeler_args['kwcount'] != 0:
         with codecs.open(scrapeler_args['scrape_save_directory'] + '\\keywords.txt', 'w', encoding="utf8") as kwf:
             kwf.write('You scraped for:\r\n')
             for tag in scrapeler_args['tags']:
@@ -288,27 +276,24 @@ def perform(scrapeler_args):
                 kwf.write('\r\nWhich found the following keyword list:\r\n')
                 for tag in sorted_related_tags[:kwcount]:
                     kwf.write('{tag} : {count}\r\n'.format(tag=tag, count=related_tags[tag]))
-    else:
-        for tag in sorted_related_tags[:kwcount]:
-            print(tag, related_tags[tag])
 
 
 def main():
     scrapeler_args = parse_scrapeler_args()
 
-    perform(scrapeler_args)
+    perform_gelbooru_scrape(scrapeler_args)
     if scrapeler_args['batch']:
         batch_file = scrapeler_args['batch']
         for command in batch_file:
             try:
-                perform(parse_scrapeler_args(command))
-                delay = _rand.uniform(900, 1200)
-                print('Sleeping for {} seconds'.format(delay))
+                perform_gelbooru_scrape(parse_scrapeler_args(command))
+                delay = random.uniform(300, 450)
+                print('Sleeping for {0} seconds between commands.'.format(delay))
                 time.sleep(delay)
             except AssertionError as ae:
                 print('Batch files may not contain calls to other batch files. Command {0} skipped.'.format(command))
-            except Exception as e:
-                print(e)
+            except Exception as ex:
+                print('Unhandled exception {e} occured during command {c}'.format(e=ex, c=command))
 
 if __name__ == '__main__':
     main()
