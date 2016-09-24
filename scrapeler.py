fcf026080c6068b2d88deaa9183769183163523f
VRushLP <rmfarc@uw.edu>
2016-09-23 20:59:32 -0700

    More minifying
    Removed sample filtering
    Webms are cast as jpgs
    Reduced delay between images.
    Images are now named by order downloaded instead of their hash.
    Images are now overridden if a file by that name already exists.
    Privatized helper functions

--- scrapeler.py
+++ scrapeler.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import os
 import random
 import re
 import time
@@ -10,7 +9,7 @@ from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
 
-def get_soup(url):
+def __get_soup(url):
     with requests.Session() as sess:
         response = sess.get(url, data=None, headers={
             'User-Agent': UserAgent().random,
@@ -24,7 +23,7 @@ def get_soup(url):
             return None
 
 
-def route_through_subpage(directory_page, subpage_id, image_file_path):
+def __route_through_subpage(directory_page, subpage_id, image_file_path):
     ret = 0
     with requests.Session() as sess:
         response = sess.get(subpage_id, data=None, headers={
@@ -38,38 +37,26 @@ def route_through_subpage(directory_page, subpage_id, image_file_path):
             img_tag = soup.find('img', id='image')
             if img_tag is not None:
                 current_img = img_tag.attrs['src']
-                if 'sample' in current_img:
-                    original_url = current_img.replace('samples', 'images').replace('sample_', '')
-                    split_url = original_url.split('.')
-                    start = response.text.find('<h5>Options</h5>')
-                    end = response.text[start:].find('>Original')
-                    search_text = response.text[start:start + end]
-                    result = search_text[search_text.find(split_url[1]):]
-                    result = result[:result.find('"')]
-                    current_img = split_url[0] + '.' + result
-
                 extension = current_img.split('?')[0][-5:].split('.')[1]
-                image_file_path = image_file_path[:-3] + extension
             else:
                 img_tag = soup.find('source')
                 current_img = img_tag.attrs['src']
-                image_file_path = image_file_path[:-3] + 'webm'
+                extension = 'jpg'
 
-            if not os.path.exists(image_file_path):
-                delay = 4 + random.uniform(3, 4)
-                time.sleep(delay)
-                ret = save_image(subpage_id, current_img, image_file_path)
+            image_file_path += extension
+
+            delay = 3 + random.uniform(2, 3)
+            time.sleep(delay)
+            ret = __save_image(subpage_id, current_img, image_file_path)
         except Exception as e:
             pass
     return ret
 
 
-def save_image(referencing_page, current_img, save_to):
+def __save_image(referencing_page, current_img, save_to):
     with requests.Session() as sess:
         response = sess.get(current_img, data=None, stream=True,
-                            headers={
-                                'User-Agent': UserAgent().firefox,
-                                'Referer': referencing_page,})
+                            headers={'User-Agent': UserAgent().firefox, 'Referer': referencing_page,})
 
         if response.status_code >= 400:
             return 0
@@ -90,38 +77,35 @@ def scrape_booru(waifu_name, scrape_save_directory):
     main_url_base = \
         r'http://gelbooru.com/index.php?page=post&s=list&tags={waifu}+rating%3aexplicit+sort%3ascore%3adesc&pid={pid}'
     referer_base = r'http://gelbooru.com/index.php?page=post&s=view&id={0}'
-    id_regex = re.compile(r'(?<=thumbnail_)([\da-f]*\.jpg|\.png|\.gif)')
     referer_regex = re.compile(r'\?[\da-f]*')
     total_saved_imgs = 0
 
     page = 1
     more_pages = True
     while total_saved_imgs < 52 and more_pages:
-        time.sleep(4 + random.uniform(0, 2))
+        time.sleep(3 + random.uniform(0, 2))
 
         scrape_url = main_url_base.format(waifu=waifu_name, pid=str(42 * (page - 1)))
-        scrape_soup = get_soup(scrape_url)
+        scrape_soup = __get_soup(scrape_url)
         results = scrape_soup.findAll('img', class_='preview')
 
         if len(results) < 42:
             more_pages = False
 
         for num, result in enumerate(results):
-            img_fn = id_regex.search(result.attrs['src']).group(1)
             refer_id = referer_regex.search(result.attrs['src']).group(0)[1:]
 
             image_file_path = "{directory}\\{fn}".format(
                 directory=scrape_save_directory,
-                fn=img_fn
+                fn='{}.'.format(total_saved_imgs + 1)
             )
             time.sleep(2 + random.uniform(0, 2))
-            total_saved_imgs += route_through_subpage(scrape_url, referer_base.format(refer_id), image_file_path)
+            total_saved_imgs += __route_through_subpage(scrape_url, referer_base.format(refer_id), image_file_path)
             if total_saved_imgs >= 52:
                 more_pages = False
                 break
 
         page += 1
-
     return total_saved_imgs
 
 
